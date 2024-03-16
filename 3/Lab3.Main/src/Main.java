26521b6c35abc37d9698abf2298f999cb22e150c
Maksim Berdnikau <153563388+d1amond09@users.noreply.github.com>
2024-03-16 23:04:05 +0300

    Delete 3/Lab3.Main directory
    Deleting directory

--- 3/Lab3.Main/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.lang.*;
-public class Main {
-   public static void main(String[] args) {
-       int[] array = {42, 17, 89, 5, 73, 28, 64, 11, 36, 50};
-       RadiusSort.sort(array);
-       System.out.println(Arrays.toString(array));
-   }
-}
